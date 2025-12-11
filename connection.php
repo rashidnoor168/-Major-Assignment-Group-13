ae5d16ef772c898d29e2f99b5370b4f414fa38ff
proman200 <hondabrandshhhhh@gmail.com>
2025-12-11 12:18:30 +0500

    docs: add documentation to database connection file
    Added comments to improve readability

--- connection.php
+++ connection.php
@@ -1,4 +1,11 @@
 <?php
+// Parking Management System
+// File: connection.php
+// Purpose: Establishes a connection to the database
+// Author: Iqrar
+// Date: (today’s date)
+// Notes: Central DB connection file used throughout the system
+
 $server="localhost";
 $username="root";
 $password="";
